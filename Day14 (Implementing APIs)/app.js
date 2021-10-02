712f26fbd203ece55acd04a9c62692ab5a4e9037
AnandDhakane01 <ananddhakane01@gmail.com>
2021-10-02 16:25:54 +0530

    tha14-tha15-tha16

--- Day14 (Implementing APIs)/app.js
+++ Day14 (Implementing APIs)/app.js
@@ -1,62 +1,65 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 const { redisClient, RedisStore, session } = require("./database/redis");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const { urlencoded } = require('express');
-var passport = require('passport');
-const { RequestTimeout } = require('http-errors');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const { urlencoded } = require("express");
+var passport = require("passport");
+const { RequestTimeout } = require("http-errors");
 
 var app = express();
 
-app.use(session({
-  store: new RedisStore({client: redisClient}), 
-  secret: 'secret#$123',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    httpOnly: false,
-    maxAge: 1000 * 60 * 10
-  }
-}))
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: "secret#$123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      maxAge: 1000 * 60 * 10,
+    },
+  })
+);
 app.use(passport.initialize());
-require("./middlewares/passport")(passport)
+require("./middlewares/passport")(passport);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/passport', require('./routes/passport'))
-app.use('/products', require("./routes/products"))
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/passport", require("./routes/passport"));
+app.use("/products", require("./routes/products"));
+app.use("/stream", require("./routes/stream"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ Day14 (Implementing APIs)/routes/stream.js
@@ -0,0 +1,40 @@
+var router = require("express").Router();
+var path = require("path");
+var fs = require("fs");
+const { builtinModules } = require("module");
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/html/index.html"));
+});
+
+router.get("/video", (req, res) => {
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send("Range is required");
+  }
+
+  const videoPath = path.join(__dirname, "../public/videos/vdoone.mp4");
+  const videoSize = fs.statSync(videoPath).size;
+
+  const CHUNK_SIZE = 10 ** 6; //1MB
+
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  const contentLength = end - start + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-type": "video/mp4",
+  };
+
+  res.writeHead(206, headers);
+
+  const videostream = fs.createReadStream(videoPath, { start, end });
+
+  videostream.pipe(res);
+});
+
+module.exports = router;
